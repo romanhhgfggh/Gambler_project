f251dd64413e76856ac6a0313698d2124e639409
romantyshchuk <roman.tychuk@gmail.com>
2026-03-26 12:23:16 +0200

    Lab 4: Created models Category, Asset, Broker and configured Admin panel

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Asset, Broker
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title') # Що показувати у списку
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    # Відображаємо назву, дату створення та оновлення, як просив викладач
+    list_display = ('title', 'category', 'price', 'created_at', 'updated_at')
+    list_filter = ('category',) # Додаємо фільтр збоку
+    search_fields = ('title', 'ticker') # Додаємо пошук
+
+@admin.register(Broker)
+class BrokerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'website', 'created_at', 'updated_at')
 # Register your models here.
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Назва категорії")
+    description = models.TextField(blank=True, verbose_name="Опис")
+
+    def __str__(self):
+        return self.title
+
+class Asset(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Назва активу")
+    ticker = models.CharField(max_length=10, verbose_name="Тікер (символ)")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='assets', verbose_name="Категорія")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Поточна ціна")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return f"{self.title} ({self.ticker})"
+
+class Broker(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Назва брокера")
+    website = models.URLField(verbose_name="Сайт")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.title
 # Create your models here.
